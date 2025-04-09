dbdfa8f50fd1862aed194ce28cb94e8874039ece
Vinícius Monteiro <vinicius@iconax.com.br>
2025-04-08 23:12:11 -0300

    feat: updating .gitignore

--- API/Program.cs
+++ API/Program.cs
@@ -10,9 +10,13 @@ builder.Services.AddDbContext<AppDbContext>(opt => {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(options => options.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:3000", "http://localhost:3000"));
+
 app.MapControllers();
 
 using var scope = app.Services.CreateScope();
