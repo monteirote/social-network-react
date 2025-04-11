43e3c688ea0ba079d56c7dbe4c4b7e475e7b5090
Vinícius Monteiro <vinicius@iconax.com.br>
2025-04-10 21:51:04 -0300

    feat: creating all CRUD operations using CQRS

---
+++ Application/Activities/Commands/DeleteActivity.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Activities.Commands;
+public class DeleteActivity {
+
+    public class Command : IRequest { 
+        public required string Id { get; set; }
+    }
+
+    public class Handler (AppDbContext context) : IRequestHandler<Command> {
+
+        public async Task Handle (Command request, CancellationToken cancellationToken) {
+
+            var activity = await context.Activities
+                .FindAsync([request.Id], cancellationToken)
+                    ?? throw new Exception("Activity not found");
+
+            context.Activities.Remove(activity);
+
+            await context.SaveChangesAsync(cancellationToken);
+        }
+    }
+
+}
+
---
+++ Application/Core/MappingProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using Domain;
+using System;
+
+namespace Application.Core;
+public class MappingProfile : Profile {
+
+    public MappingProfile() {
+        CreateMap<Activity, Activity>();
+    }
+
+}
+
